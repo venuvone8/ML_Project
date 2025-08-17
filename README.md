3c8fec8a535167c4a708a2579ea6849472e8beb5
venuvone8 <venuvone8@gmail.com>
2025-08-17 11:45:04 +0530

    EDA and problem statement

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## End to End Machine Learning Projrct
+## End to End Machine Learning Project
+Step1: Setup all the requirements
+Step2:
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
-import setuptools
+from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e.'
-def get_requirements(file_path:str)->List[str]:
+HYPEN_E_DOT = '-e.'
+
+def get_requirements(file_path: str) -> List[str]:
     '''
-    this function will return the list of requirements
+    This function will return the list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -22,6 +22,8 @@ setup(
     version='0.0.1',
     author='Venu',
     author_email='venuvone8@gmail.com',
-    packages=setuptools.find_namespace_packages(),
-    install_requires=get_requirements('requirements.txt')
+    packages=find_packages(),  # Use find_packages() for regular packages
+    install_requires=get_requirements('requirements.txt'),
+    long_description=open('README.md').read(),  # Optional: Add long description
+    long_description_content_type='text/markdown',  # Optional: Specify the content type
 )
